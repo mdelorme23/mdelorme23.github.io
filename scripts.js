da19d0a533de79160fdf85d2a6aa168b4f6c2ba4
Martin Delorme <mdelorme@innobec.com>
2016-01-04 18:47:29 -0500

    fix

--- scripts.js
+++ scripts.js
@@ -2,5 +2,5 @@ $('.tabs li').on('click', function(){
 	$('.showcase, .tabs').children('.active').removeClass('active');
 	var clickedClass = $(this).attr("class");
 	$(this).addClass('active');
-	$('.showcase').addClass('active');
+	$('.showcase').children('.' + clickedClass).addClass('active');
 });
