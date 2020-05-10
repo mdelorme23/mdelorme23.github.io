9c8afd5347e2a18262ca422ecf30bdfae9621924
Martin Delorme <mdelorme@innobec.com>
2020-05-10 14:11:54 -0400

    Clean up

--- scripts.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-	$('.sections > span').click(function(){
-		if(!$(this).hasClass('active')){
-			$('.sections > span').removeClass("active");
-			$(this).addClass('active');
-			$('.contained1').toggle();
-		}
-	});
-});
